f0d1009abfd7b405ed5462eb6034c8c994e3529c
RiP3rQ <rafalpompa2000@gmail.com>
2023-03-18 21:27:52 +0100

    animate header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -7,9 +8,24 @@ const Header = (props: Props) => {
   return (
     <header
       className="sticky top-0 p-5 flex items-start justify-between 
-    max-w-7xl mx-auto z-20 xl:items-center"
+    max-w-7xl mx-auto z-20 xl:items-center overflow-x-hidden"
     >
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: -500,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center"
+      >
         {/* SOCIAL ICONS | LEFT*/}
         <SocialIcon
           url="https://github.com/rip3rq"
@@ -21,9 +37,25 @@ const Header = (props: Props) => {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
+
       {/* RIGTH */}
-      <div className="flex flex-row items-center text-gray-300 cursor-pointer">
+      <motion.div
+        initial={{
+          opacity: 0,
+          x: 500,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -33,7 +65,7 @@ const Header = (props: Props) => {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
           Get in touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 };
