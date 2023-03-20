4764a5404b954b049233714ef5067adb7ea43029
RiP3rQ <rafalpompa2000@gmail.com>
2023-03-20 20:03:11 +0100

    hotfix for hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = ({ pageInfo }: Props) => {
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px] ">
           {pageInfo?.role}
         </h2>
-        <h1 className="text-3xl md:text-4xl xl:text-6xl font-semibold px-10">
+        <h1 className="text-2xl md:text-4xl xl:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
