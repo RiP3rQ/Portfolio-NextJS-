4ecff10bd03041c0bd834a247f3b897864f6a1d6
RiP3rQ <rafalpompa2000@gmail.com>
2023-03-20 20:01:37 +0100

    hero section improvements for mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,10 +28,10 @@ const Hero = ({ pageInfo }: Props) => {
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
       />
       <div className="z-20">
-        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px] ">
           {pageInfo?.role}
         </h2>
-        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+        <h1 className="text-3xl md:text-4xl xl:text-6xl font-semibold px-10">
           <span className="mr-3">{text}</span>
           <Cursor cursorColor="#F7AB0A" />
         </h1>
